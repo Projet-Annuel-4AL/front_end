31f0a4d3818c4f45e5d3317b0660c8c053d3679a
LAN <lucasangostonpro@gmail.com>
2022-05-02 00:52:09 +0200

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CodeRunnerComponent } from './code-runner/code-runner.component';
+import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MonacoEditorModule} from "ngx-monaco-editor";
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeRunnerComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MonacoEditorModule.forRoot(),
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

ba160fac2d9d2c7daca0684988fcc6ca3755b439
LAN <lucasangostonpro@gmail.com>
2022-05-07 19:58:59 +0200

    lazy loading and shared directory

--- src/app/Authentication/authentication.module.ts
+++ src/app/Authentication/authentication.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-
-
+import { AuthRoutingModule } from './auth-routing.module';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
+    SharedModule,
+    AuthRoutingModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,10 @@ import { PostModule } from "./post/post.module";
 import { CreatePostComponent} from "./post/components/create-post/create-post.component";
 import { ListPostComponent } from "./post/components/list-post/list-post.component";
 import { CodeRunnerComponent } from './post/components/submit-post/code-runner/code-runner.component';
-import { LoginComponent } from './Authentication/components/login/login.component';
 import { PageNotFoundComponent } from './errors-page/components/page-not-found/page-not-found.component';
-import { RegisterComponent } from './Authentication/components/register/register.component';
 import { SubmitPostComponent } from './post/components/submit-post/submit-post.component';
 import { SubmitTextComponent } from './post/components/submit-post/submit-text/submit-text.component';
 import { SubmitImageVideoComponent } from './post/components/submit-post/submit-image-video/submit-image-video.component';
-import {MatCardModule} from "@angular/material/card";
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import {TokenInterceptor} from "./token-interceptor";
 
@@ -31,9 +24,7 @@ import {TokenInterceptor} from "./token-interceptor";
   declarations: [
     AppComponent,
     CodeRunnerComponent,
-    LoginComponent,
     PageNotFoundComponent,
-    RegisterComponent,
     SubmitPostComponent,
     SubmitTextComponent,
     SubmitImageVideoComponent,
@@ -49,11 +40,6 @@ import {TokenInterceptor} from "./token-interceptor";
     BrowserAnimationsModule,
     SharedModule,
     CoreModule,
-    MatCardModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
     NgxDropzoneModule,
     PostModule
   ],

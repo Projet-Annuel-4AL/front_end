9f9db5c44e63d83d781b58394f28903897b8a543
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-27 23:42:54 +0200

    add code execution button

--- src/app/post/components/remarks-post/remarks-post.component.ts
+++ src/app/post/components/remarks-post/remarks-post.component.ts
@@ -4,6 +4,7 @@ import {PostService} from "../../service/post.service";
 import {Post} from "../../domain/post.entity";
 import {Remark} from "./domain/remark.entity";
 import {RemarksPostService} from "./service/remarks.post.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-remarks-post',
@@ -13,23 +14,52 @@ import {RemarksPostService} from "./service/remarks.post.service";
 export class RemarksPostComponent implements OnInit {
   post!: Post;
   remarks!: Remark[];
+  language!: string;
+  theme!: string;
+  code!: string;
+  output!: string;
   editorOptions!: any;
 
   constructor(
     private _postService: PostService,
     private _activatedRoute: ActivatedRoute,
-    private _remarksService: RemarksPostService
+    private _remarksService: RemarksPostService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
     const routeParams = this._activatedRoute.snapshot.paramMap;
     const idPost = Number(routeParams.get('idPost'));
-    this.editorOptions = {readOnly: true, theme: 'vs-dark', language: 'java', automaticLayout: true};
+    this.language = 'java';
+    this.theme = 'vs-dark'
+    this.editorOptions = {readOnly: true, theme: this.theme, language: this.language, automaticLayout: true};
 
     this._postService.getPostById(idPost).subscribe(post => {
       this.post = post;
       this.remarks = this.post.remarks;
+      if (post.code !== null){
+        this.code = post.code.content;
+      }
     });
   }
 
+  getLangNumber() {
+    if (this.language == 'python') return 1;
+    if (this.language == 'cpp') return 2;
+    return 0;
+  }
+
+  onRunCode(){
+    this.getLangNumber()
+    const body = {language: this.getLangNumber(), code: this.code};
+
+    this.http
+      .post( "http://localhost:3000/api/compiler",body, {responseType: 'text'}).toPromise()
+      .then(response => {
+        this.output = response;
+      })
+      .catch( err => {
+        console.log(err)
+      });
+  }
 }

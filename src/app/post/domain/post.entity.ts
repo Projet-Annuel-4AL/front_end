50149a9b0dbc689769dcb61a5680954f09c5931f
Ilyess Naït Belkacem <iilyessnb@gmail.com>
2022-06-27 21:32:09 +0200

    add remarks: Remark[] attribute in post.entity.ts

--- src/app/post/domain/post.entity.ts
+++ src/app/post/domain/post.entity.ts
@@ -1,6 +1,7 @@
 import {User} from "../../user/domain/user.entity";
 import {Text} from "../post-body/domain/text.entity";
 import {Code} from "../post-body/domain/code.entity";
+import {Remark} from "../components/remarks-post/domain/remark.entity";
 
 export class Post {
   idPost: number;
@@ -11,8 +12,9 @@ export class Post {
   text: Text;
   code: Code;
   user: User;
+  remarks: Remark[];
 
-  constructor(idPost: number,title: string, idVideo: number, idPicture: number, text: Text, code: Code, user: User) {
+  constructor(idPost: number,title: string, idVideo: number, idPicture: number, text: Text, code: Code, user: User, remarks: Remark[]) {
     this.idPost = idPost;
     this.title = title;
     this.createdDate = new Date();
@@ -21,5 +23,6 @@ export class Post {
     this.text = text;
     this.code = code;
     this.user = user;
+    this.remarks = remarks
   }
 }

65cdba5ebd087785f829311624d0e78e91460f79
lucasangoston <lucasangostonpro@gmail.com>
2022-06-27 10:30:53 +0200

    fix bug no display remark space

--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CommentComponent} from "./components/comment/comment.component";
 import {SubmitPostComponent} from "./components/submit-post/submit-post.component";
 import {PostComponent} from "./post.component";
+import {RemarksPostComponent} from "./components/remarks-post/remarks-post.component";
 
 const routes: Routes = [
-  { path: '', component: PostComponent },
+  { path: '', component: PostComponent},
   { path: 'submit', component: SubmitPostComponent },
-  { path: 'comment', component: CommentComponent }
+  { path: 'remark', component: RemarksPostComponent }
 ];
 
 @NgModule({

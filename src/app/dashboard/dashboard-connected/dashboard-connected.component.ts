c80154e34ae321cfd32a47e961a74931599cc370
MrPuel <theo.gouraud@inetum.com>
2022-07-08 02:03:08 +0200

    add deconnection

--- src/app/dashboard/dashboard-connected/dashboard-connected.component.ts
+++ src/app/dashboard/dashboard-connected/dashboard-connected.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {JwtTokenService} from "../../Authentication/services/jwt-token.service";
+import {LocalStorageService} from "../../Authentication/services/local-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-connected',
@@ -9,11 +11,20 @@ import {JwtTokenService} from "../../Authentication/services/jwt-token.service";
 export class DashboardConnectedComponent implements OnInit {
   idUser! : number
 
-  constructor(private _jwtTokenService: JwtTokenService) {
+  constructor(private _jwtTokenService: JwtTokenService, private _localStorageService: LocalStorageService, private _router: Router) {
     this.idUser = Number(_jwtTokenService.getIdUser())
   }
 
   ngOnInit(): void {
+  }
+
+  deconnection(): void {
+    this._jwtTokenService.getDecodeToken()
+    this._localStorageService.remove("JWTToken");
+    this._router.navigateByUrl("")
+      .then(() => {
+        window.location.reload();
+      });
   }
 
 }

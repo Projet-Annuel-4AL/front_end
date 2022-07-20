6c59fdf101b3ceac4029948a2f590b0f2ddc7504
lucasangoston <lucasangostonpro@gmail.com>
2022-07-20 13:31:23 +0200

    deploy front

--- src/app/apiUrlConstant.ts
+++ src/app/apiUrlConstant.ts
@@ -1,5 +1,5 @@
 export class ApiUrlConstant {
   //52.208.34.20:3000
   //localhost:3000
-  static readonly HOST: string = "localhost:3000/api/";
+  static readonly HOST: string = "http://52.208.34.20:3000/api/";
 }
